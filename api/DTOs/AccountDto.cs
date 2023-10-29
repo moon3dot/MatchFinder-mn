a5a4189e918d32fe9734ae6b9d669e752cb6fe9e
Moon3dot <moon3dot@gmail.com>
2023-10-29 14:58:50 +0330

    Creat Dto for Accoutns

--- api/DTOs/AccountDto.cs
+++ api/DTOs/AccountDto.cs
@@ -1,11 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.DTOs;
 
-public class AccountDto
-{
-    
-}
+public record RegisterDto(
+     // Email
+     [MaxLength(30), RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$", ErrorMessage = "This email format is not correct")]
+     string Email,
+     //password
+     [DataType(DataType.Password), MinLength(7) , MaxLength(30)]
+     string Password,
+     //confirm Password
+     [DataType(DataType.Password), MinLength(7) , MaxLength(30)]
+     string ConfirmPassword
+);
+
+public record LoginDto(
+     // Email
+     [MaxLength(30), RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$", ErrorMessage = "This email format is not correct")]
+     string Email,
+     // Password
+     [DataType(DataType.Password), MinLength(7) , MaxLength(30)]
+     string Password
+);
+
+public record LoggedInDto(
+     // Id make Api
+     string Id,
+     // Email user in mongoDB
+     string Email,
+     // Token make Api
+     string Token
+);
